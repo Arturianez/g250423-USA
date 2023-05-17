01761b699d284e8445d92e9207dbe4bfabbdc0b8
Arturianez <artur.zank@gmail.com>
2023-05-17 17:14:02 -0400

    Test

---
+++ src/main/java/lesson5/task2/IfElseTest.java
@@ -0,0 +1,16 @@
+package lesson5.task2;
+
+public class IfElseTest {
+    public static void main(String[] args) {
+
+        int a = 15;
+        int b = 10;
+
+        if (a < b) {
+            System.out.println("А меньше чем В");
+        } else {
+            System.out.println("А больше чем B");
+        }
+
+    }
+}
---
+++ src/main/java/lesson5/task2/IfTest.java
@@ -0,0 +1,18 @@
+package lesson5.task2;
+
+public class IfTest {
+    public static void main(String[] args) {
+
+        System.out.println("Начало");
+
+        int a = 10;
+        int b = 15;
+        boolean result = a > b;
+
+        if (result) {
+            System.out.println("А больше, чем В");
+        }
+        System.out.println("Конец");
+    }
+
+}
